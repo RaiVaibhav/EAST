da0f89df10852ba4c4b55557a85e0e91140fe2c2
Yash Srivastava <yashsrivastava3005@gmail.com>
2017-10-23 21:58:18 +0530

    Modified Checkpoint Path

--- run_demo_server.py
+++ run_demo_server.py
@@ -191,7 +191,7 @@ def save_result(img, rst):
 
 
 
-checkpoint_path = '~/GSOC/tmp/east_icdar2015_resnet_v1_50_rbox'
+checkpoint_path = '~/GSOC/tmp/east_model/east_icdar2015_resnet_v1_50_rbox'
 
 
 @app.route('/', methods=['POST'])
