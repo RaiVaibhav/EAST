101d29998f514c41a21ea9597000f48768255bdc
Yash Srivastava <yashsrivastava3005@gmail.com>
2017-10-24 22:18:42 +0530

    Output Image.

--- run_demo_server.py
+++ run_demo_server.py
@@ -181,8 +181,7 @@ def save_result(img, rst):
     # save illustration
     output_path = os.path.join(dirpath, 'output.png')
     cv2.imwrite(output_path, draw_illu(img.copy(), rst))
-    img1 = draw_illu(img.copy(), rst)
-    origami.sendImageArray([img1],mode = 'numpy_array')
+    origami.sendImageArray([output_path],mode = 'file_path')
 
     # save json data
     output_path = os.path.join(dirpath, 'result.json')
